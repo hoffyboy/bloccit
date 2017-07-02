6ac75b0a494528353cf6fb5398f1d7019502f922
Zalmi Hofstatter <hoffyboy@gmail.com>
2017-07-02 03:03:15 -0700

    assignment-23 complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe User, type: :model do
   # Shoulda tests for name
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
-
   # Shoulda tests for email
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email) }
@@ -20,6 +19,12 @@ RSpec.describe User, type: :model do
   describe "attributes" do
     it "should have name and email attributes" do
       expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
+    end
+
+    it "should format the user's name" do
+      user.name = "bloc user"
+      user.save
+      expect(user.name).to eq("Bloc User")
     end
   end
 
